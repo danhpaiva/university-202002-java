ebbf54ee1641452b3da4628fe27c912fa6804cee
Daniel Paiva <danpaiva@live.com>
2020-09-12 12:55:11 -0300

    implement scan numbers

--- calculator-RMI/CalculadoraClient.java
+++ calculator-RMI/CalculadoraClient.java
@@ -1,17 +1,46 @@
 import java.rmi.*;
+import java.util.Scanner;
 
 public class CalculadoraClient{
   
   public static void main (String args[]) throws RemoteException
   {
-    int resultado;
-    
+    int operacao, resultado;
+
     try {
-     
-	  Calculadora obj = (Calculadora) Naming.lookup("//localhost/CalculadoraServer");
-  	  resultado = obj.somar(10, 5);
-  	  System.out.println(resultado);
+      Scanner scan = new Scanner(System.in);
+      Calculadora obj = (Calculadora) Naming.lookup("//localhost/CalculadoraServer");
+
+      System.out.println("Qual operação deseja realizar: [1]Soma [2]Subtração [3]Multiplicação [4]Divisão");
+      operacao = Integer.valueOf(scan.nextLine());
+
+      System.out.println("Informe o primeiro valor: ");
+      int valor1 = Integer.valueOf(scan.nextLine());
       
+      System.out.println("Informe o segundo valor: ");
+      int valor2 = Integer.valueOf(scan.nextLine());
+      
+      switch (operacao) {
+        case 1:
+          resultado = obj.somar(valor1, valor2);
+          System.out.println("\nResultado: " + resultado);
+          break;
+        case 2:
+          resultado = obj.subtrair(valor1, valor2);
+          System.out.println("\nResultado: " + resultado);
+          break;
+        case 3:
+          resultado = obj.multiplicar(valor1, valor2);
+          System.out.println("\nResultado: " + resultado);
+          break;
+        case 4:
+          resultado = obj.dividir(valor1, valor2);
+          System.out.println("\nResultado: " + resultado);
+          break;
+        default:
+          System.out.println("\nOperação inválida!");
+          break;
+      } 
     } catch (Exception e) {
        System.err.println("Falha de conexao : " + e.toString());
     }
--- calculator-RMI/CalculadoraServer.java
+++ calculator-RMI/CalculadoraServer.java
@@ -9,24 +9,29 @@ public class CalculadoraServer extends UnicastRemoteObject implements Calculador
   public CalculadoraServer() throws RemoteException
   { 
   	  super(); 
-	  System.out.println("Criada uma instancia desta classe...");
+	    System.out.println("Criada uma instancia desta classe...");
   }
+
   public int somar(int numero1, int numero2)  throws RemoteException
   {
-	  return (numero1 + numero2);
+	  return numero1 + numero2;
   }
+
   public int subtrair(int numero1, int numero2)  throws RemoteException
   {
-	  return (numero1 - numero2);
+	  return numero1 - numero2;
   }
+
   public int multiplicar(int numero1, int numero2)  throws RemoteException
   {
-	  return (numero1 * numero2);
+	  return numero1 * numero2;
   }
+
   public int dividir(int numero1, int numero2)  throws RemoteException
   {
-	  return (numero1 / numero2);
+	  return numero1 / numero2;
   }
+
   public static void main (String args[]) throws Exception 
   {
 	  CalculadoraServer objMensagem = new CalculadoraServer();
